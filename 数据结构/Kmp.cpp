482b35d8ef69db7068bbd2cf75c61aa59ef9f6b2
cccccly <919345318@qq.com>
2018-03-26 21:53:21 +0800

    1
    1

--- 数据结构/Kmp.cpp
+++ 数据结构/Kmp.cpp
@@ -40,17 +40,41 @@ int nood_match(PSeqString t,PSeqString p){
 	else return 0;
 }
 
-int *makeNext(PSeqString p,int *next){
+void *makeNext(PSeqString p,int *next){
 	int i =0,k = -1;
 	next[0] = -1;
 	while(i < p->n-1){
-		
+		while(k >= 0 && p->c[i]!=p->c[k])
+			k = next[k];
+		i++,k++;
+		next[i] = k;
 	}
 } 
+
+int KMP(PSeqString t,PSeqString p,int *next){
+	int i,j;
+	i = 0,j = 0;
+	while(i < p->n && j< t->n)
+		if(i == -1||p->c[i] == t->c[i]){
+			i++;
+			j++;
+		}
+}
 int main()
 {
 	double start,finish;
 	start = clock();
+	PSeqString pstr1 = createNullStr_seq(8);
+	char c[] = {'B','A','B','B','A','B','A','B'};
+	for(int i = 0;i < pstr1->n;i++){
+		pstr1->c[i] = c[i];
+		pstr1->n++;
+	}
+	int *next = (int *)malloc(sizeof(int)*pstr1->n);
+	makeNext(pstr1,next);
+	for(int i = 0;i < pstr1->n;i++)
+		printf("%d",next[i]);
+		
 	
 	finish = clock();
 	printf("time is %f",(finish-start)/CLOCKS_PER_SEC);
